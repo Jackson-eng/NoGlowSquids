372503415d0c816cb194ffa36e7521f84737370b
Jackson <71522825+Jackson-eng@users.noreply.github.com>
2021-08-31 13:33:47 -0700

    Update Main.java

--- src/me/jacksondasheng/noGlowSquids/Main.java
+++ src/me/jacksondasheng/noGlowSquids/Main.java
@@ -14,7 +14,7 @@ public class Main extends JavaPlugin
     @Override
     public void onEnable()
     {
-        server.getConsoleSender().sendMessage("NoGlowSquids enabled");
+        server.getConsoleSender().sendMessage("Enabling NoGlowSquids");
 
         server.getPluginManager().registerEvents(new AntiGlowSquid(), this);
         server.getScheduler().scheduleSyncRepeatingTask(
